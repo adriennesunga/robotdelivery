f6779ee97f6f9389f4cd4c21e53ca3ec04804429
Adrienne Sunga <adriennesunga@gmail.com>
2018-08-03 12:03:22 -0400

    added a second robot, and comments

--- game.js
+++ game.js
@@ -2,18 +2,20 @@ const origin = {x: 0, y: 0}
 // robots always start at 0,0 
 
 var housesTotal = document.querySelector('.houses-total')
-
-
 var currentPosition = document.querySelector('.current-position')
+var currentPosition2 = document.querySelector('.current-position2')
 
 var robbieRobot = {
     name: "Robbie",
     lastMove: origin,
 }
 // declare a unique variable
-// name property to use in html
 // lastmove = origin , all robots always start at 0,0
 
+var jillRobot = {
+    name: "Jill",
+    lastMove: origin,
+}
 
 function move(robot, direction){
      if (direction  == 'up'){
@@ -24,7 +26,9 @@ function move(robot, direction){
         // adding to the y coordinate by 1 (moves up)
         addToVisited(housesVisited, robot.lastMove)
         displayLastMove(robot.lastMove.x, robot.lastMove.y)
+        displayLastMove2(robot.lastMove.x, robot.lastMove.y)
         // pushes to the array of houses visited, if no duplicates
+        //updates coordinates as (last move)
     } else if(direction == 'down'){
         robot.lastMove = {
             x: robot.lastMove.x,
@@ -32,6 +36,7 @@ function move(robot, direction){
         }
         addToVisited(housesVisited, robot.lastMove)
         displayLastMove(robot.lastMove.x, robot.lastMove.y)
+        displayLastMove2(robot.lastMove.x, robot.lastMove.y)
     } else if (direction == 'left'){
         robot.lastMove = {
             x: robot.lastMove.x -1,
@@ -39,6 +44,7 @@ function move(robot, direction){
         }
         addToVisited(housesVisited, robot.lastMove)
         displayLastMove(robot.lastMove.x, robot.lastMove.y)
+        displayLastMove2(robot.lastMove.x, robot.lastMove.y)
     }else if (direction == 'right'){
         robot.lastMove = {
             x: robot.lastMove.x +1,
@@ -46,6 +52,7 @@ function move(robot, direction){
         }
         addToVisited(housesVisited, robot.lastMove)
         displayLastMove(robot.lastMove.x, robot.lastMove.y)
+        displayLastMove2(robot.lastMove.x, robot.lastMove.y)
     }else {}
     
 }
@@ -54,9 +61,11 @@ function addToVisited(houses, lastMove){
     var exists = houses.filter(function(house){
         return house.x == lastMove.x &&
                house.y == lastMove.y 
+// filters to check if coordinates have been landed on before
     })
     if (exists.length ==0){
         housesVisited.push(lastMove)
+// if they haven't been landed on, push the lastMove to the housesVisited array
     }
     housesTotal.innerHTML = housesVisited.length
     // force update anytime function runs
@@ -65,19 +74,23 @@ function addToVisited(houses, lastMove){
 
 function clickNext() {
     move(robbieRobot, "up")
-    setTimeout(function(){ move(robbieRobot, "up"); }, 2000);
+    setTimeout(function(){ move(jillRobot, "up"); }, 2000);
     setTimeout(function(){ move(robbieRobot, "down"); }, 3000);
-    setTimeout(function(){ move(robbieRobot, "down"); }, 4000);
+    setTimeout(function(){ move(jillRobot, "down"); }, 4000);
     setTimeout(function(){ move(robbieRobot, "left"); }, 5000);
-    setTimeout(function(){ move(robbieRobot, "right"); }, 6000);
+    setTimeout(function(){ move(jillRobot, "right"); }, 6000);
+    
   }
-//set timeout function to show each coordinate
+//set timeout function to show each coordinate in html
 
 var housesVisited = []
 
 function displayLastMove(x,y){
-    return currentPosition.innerHTML = "x: " + x + " y: " + y
+    return currentPosition.innerHTML = ("x: " + robbieRobot.lastMove.x) + (" y: " + robbieRobot.lastMove.y)
 }
 
+function displayLastMove2(x,y){
+    return currentPosition2.innerHTML = ("x: " + jillRobot.lastMove.x) + (" y: " + jillRobot.lastMove.y)
+}
 
 
